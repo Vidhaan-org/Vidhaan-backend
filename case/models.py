b0768cf9de5f317efa2e5a65c2c309d8cfcd1397
Sonal Jain <sonaljain067@gmail.com>
2022-08-05 00:37:18 +0530

    reversing case table relations

--- case/models.py
+++ case/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from petitionFiling.models import *
+from permuser.models import * 
+
 from django.contrib.auth import get_user_model
 Users=get_user_model()
 
+class Case(models.Model):
+    cnr_number=models.IntegerField(unique=True, null=True)
+    case_type=models.CharField(max_length=50, null=True,blank=True,choices=choice.CASE_TYPE)
+    filling_number=models.IntegerField(null=True)
+    registration_number=models.IntegerField(null=True)
+
+
 class IADetails(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_ia_details')
     ia_number=models.CharField(null=True,blank=True, max_length=50)
     party=models.ForeignKey(to=Users,null=True,blank=True,default="",on_delete=models.CASCADE)
     filing_date=models.DateField(null=True,blank=True,default=0)
@@ -12,11 +22,13 @@ class IADetails(models.Model):
 
 class Order(models.Model): 
     # id=order no (for now)
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_order')
     judge=models.ForeignKey(to=Judge,null=True,blank=True,default="",on_delete=models.CASCADE)
     order_date=models.DateField(null=True,blank=True,default=0)
 
 
 class Objection(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_objection')
     scrutiny_date=models.DateField(null=True,blank=True,default=0)
     objection=models.CharField(max_length=500, null=True,blank=True)
     compliance_date=models.DateField(null=True,blank=True,default=0)
@@ -24,6 +36,7 @@ class Objection(models.Model):
 
 
 class DocumentDetails(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_document')
     document_no=models.IntegerField(null=True,blank=True,default=0)
     recieving_date=models.DateField(null=True,blank=True,default=0)
     filed_by=models.ForeignKey(to=Users,null=True,blank=True,default="",on_delete=models.CASCADE)
@@ -31,6 +44,7 @@ class DocumentDetails(models.Model):
     document=models.FileField(upload_to="document/",null=True, blank=True)
 
 class History(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_history')
     cause_list_type=models.CharField(max_length=50, null=True,blank=True)
     judge=models.ForeignKey(to=Judge,null=True,blank=True,default="",on_delete=models.CASCADE)
     hearing_date=models.DateField(null=True,blank=True,default=0)
@@ -38,8 +52,9 @@ class History(models.Model):
 
 
 class CaseStatus(models.Model):
-    stage_of_case=models.CharField(max_length=50, null=False, blank=True)
-    coram=models.CharField(max_length=50, null=False, blank=True,default="")
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_status')
+    stage_of_case=models.CharField(max_length=50, null=True, blank=True)
+    coram=models.CharField(max_length=50, null=True, blank=True,default="")
     bench=models.CharField(max_length=50, null=True,blank=True,default="")
     state=models.CharField(max_length=50, null=True,blank=True,default="")
     district=models.CharField(max_length=50, null=True,blank=True,default="")
@@ -48,25 +63,54 @@ class CaseStatus(models.Model):
 
     latest_hearing=models.DateField(null=True,blank=True,default="")
     decision_date=models.DateField(null=True,blank=True,default="")
-    case_status=models.CharField(max_length=50, null=False, choices=choice.CASE_STATUS)
+    case_status=models.CharField(max_length=50, null=True, choices=choice.CASE_STATUS)
     next_date=models.DateField(null=True,blank=True,default="") 
 
-class Case(models.Model):
-    cnr_number=models.IntegerField(unique=True, null=False)
-    case_type=models.CharField(max_length=50, null=True)
-    filling_number=models.IntegerField(null=False)
-    registration_number=models.IntegerField(null=False)
-    registration_number=models.DateField(null=True)
-    case_status=models.ForeignKey(to=CaseStatus,null=True,blank=True,default="",on_delete=models.CASCADE)
-    petitioner=models.ManyToManyField(Petitioner,null=True,blank=True)
-    respondent=models.ManyToManyField(Respondent,null=True,blank=True)
-    advocate=models.ManyToManyField(Advocate,null=True,blank=True)
-    act=models.ManyToManyField(Act,null=True,blank=True)
-    ia=models.ManyToManyField(IADetails,null=True,blank=True)
-    history=models.ManyToManyField(History,null=True,blank=True)
-    order=models.ManyToManyField(Order,null=True,blank=True)
-    objection=models.ManyToManyField(Objection,null=True,blank=True)
-    document=models.ManyToManyField(DocumentDetails,null=True,blank=True)
 
+class CasePetitioner(models.Model):
+    case=models.ForeignKey(to=Case,default="",on_delete=models.CASCADE,related_name='case_petitioner')
+    petitioner_type = models.CharField(null=True,blank=True,choices=choice.PETITION_TYPE,default="",max_length=50)
+    petitioner_name = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_age = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_state = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_address = models.CharField(null=True,blank=True, max_length=200)
+    petitioner_country = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_mobile = models.IntegerField(null=True,blank=True)
+    petitioner_department = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_city = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_email = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_pin = models.IntegerField(null=True,blank=True)
+    petitioner_district = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_total_petition = models.IntegerField(null=True,blank=True)
+
+class CaseRespondent(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_respondent')
+    respondent_name = models.CharField(null=True,blank=True, max_length=50)
+    respondent_relation = models.CharField(null=True,blank=True, max_length=50)
+    respondent_father = models.CharField(null=True,blank=True, max_length=50)
+    respondent_gender = models.CharField(null=True,blank=True, max_length=50)
+    respondent_address = models.TextField(null=True,blank=True, max_length= 200)
+    respondent_country = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_city = models.CharField(null=True,blank=True, max_length=50)
+    petitioner_email = models.CharField(null=True,blank=True, max_length=50)
+
+
+class CaseAct(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_act')
+    petition=models.ForeignKey(to=Petition,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_petitioner')
+    act_title=models.CharField(null=True,blank=True, max_length=150)
+    act_rule=models.CharField(null=True,blank=True, max_length=150)
+    section=models.CharField(null=True,blank=True, max_length=450)
+    rule_no=models.IntegerField(null=True,blank=True)
+    act_belong_to=models.CharField(null=True,blank=True, max_length=50,choices=choice.ACT_BELONG_TO)
+
+
+class Notification(models.Model):
+    case=models.ForeignKey(to=Case,null=True,blank=True,default="",on_delete=models.CASCADE,related_name='case_notifications')
+    title=models.CharField(max_length=250, null=True,blank=True)
+    mail_description=models.CharField(max_length=5000, null=True,blank=True)
+    web_description=models.CharField(max_length=500, null=True,blank=True)
+    is_viewed_in_mail=models.BooleanField(null=True,blank=True,default=False)
+    is_viewed_in_web=models.BooleanField(null=True,blank=True,default=False)
 
 
--- case/views.py
+++ case/views.py
@@ -1,8 +1,12 @@
+from typing import List
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class CaseDetail(APIView):
     def post(self,request):
@@ -19,7 +23,10 @@ class CaseDetail(APIView):
                 "data": serializer.errors
             })
     def get(self,request,id=None):
-        if id:
+        search_query = self.request.query_params.get('search_query') or None
+        filter_query=self.request.query_params.get('filter_query') or None
+
+        if not id:
             try:
                 case=Case.objects.get(id=id)
                 serializer=CaseSerializer(case)
@@ -32,16 +39,35 @@ class CaseDetail(APIView):
                     "status_code": 400,
                     "data": serializer.errors
                 })
-        try:
-            cases=Case.objects.all()
-            serializer=CaseSerializer(cases,many=True)
-            return Response({
-                "status_code": 200,
-                "data": serializer.data
-            })
-        except ObjectDoesNotExist:
-            return Response({
-                "status_code": 400,
-                "data": serializer.errors
-            })
+        # elif search_query :
+        #     queryset = queryset.filter(purchaser__username=search_query)
+        #     return queryset
+        elif filter_query: 
+            queryset = queryset.filter(purchaser__username=search_query)
+            return queryset
+        else: 
+            try:
+                cases=Case.objects.all()
+                serializer=CaseSerializer(cases,many=True)
+                return Response({
+                    "status_code": 200,
+                    "data": serializer.data
+                })
+            except ObjectDoesNotExist:
+                return Response({
+                    "status_code": 400,
+                    "data": serializer.errors
+                })
+        
+            
+class CaseList(ListAPIView):
+    serializer_class=CaseSerializer
+
+    def get_queryset(self):
+        queryset=Case.objects.all()
+        query=self.request.query_params.get('query')
+        if query is not None:  
+            return queryset.filter(case_status_details__case_status__icontains=query) or queryset.filter(cnr_number__iexact=query) or queryset.filter(petitioner__petitioner_name__icontains=query) or queryset.filter(respondent__respondent_name__icontains=query) 
+        else: 
+            return queryset
 
