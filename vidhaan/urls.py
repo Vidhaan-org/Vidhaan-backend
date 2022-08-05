6ba2fb2bc65c18b6ea56ad61c712a9375748a52a
Suryansh Bisen <69685349+Suryansh1191@users.noreply.github.com>
2022-08-05 16:08:28 +0530

    JWT Auth

--- vidhaan/urls.py
+++ vidhaan/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('file_petition/',include('petitionFiling.urls')),
-     path('case/',include('case.urls'))
+    path('case/',include('case.urls')),
+    path('api/token/verify/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair')
 ]
