67aee85e781f99795edeb8dffa35134437e379ef
rahulkesharwani353 <rahulkesharwani353@gmail.com>
2022-08-08 23:30:26 +0530

    models updated

--- permuser/admin.py
+++ permuser/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import UserPermission,EmployeeModel
+from .models import *
 
 
 # Register your models here.
 
 admin.site.register(UserPermission)
 admin.site.register(EmployeeModel)
+admin.site.register(Petitioner)
+admin.site.register(Respondent)
+admin.site.register(Act)
+admin.site.register(Advocate)
--- petitionFiling/urls.py
+++ petitionFiling/urls.py
@@ -5,8 +5,8 @@ namespace=['file_petition']
 
 urlpatterns=[
     path('',FilePetition.as_view()),
-    path('add_petitioner/',Petitioner.as_view()),
-    path('add_respondent/',Respondent.as_view()),
-    path('add_act/',Act.as_view()),
-    path('add_advocate/',Advocate.as_view())
+    # path('add_petitioner/',Petitioner.as_view()),
+    # path('add_respondent/',Respondent.as_view()),
+    # path('add_act/',Act.as_view()),
+    # path('add_advocate/',Advocate.as_view())
 ]
