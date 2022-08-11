9646ba8a8e689640fc9bcec906ea68c00026c36e
Sonal Jain <sonaljain067@gmail.com>
2022-08-11 18:47:09 +0530

    changing settings.py

--- vidhaan/settings.py
+++ vidhaan/settings.py
@@ -153,10 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, 'staticfiles'),)
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# ]
 DISABLE_COLLECTSTATIC: 1
 
 # Default primary key field type
