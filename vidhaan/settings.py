54d7009f7f1d07a45eb9bfc2f64543092c0f5ccf
Sonal Jain <sonaljain067@gmail.com>
2022-08-11 20:37:18 +0530

    adding whitenoise storage

--- vidhaan/settings.py
+++ vidhaan/settings.py
@@ -59,8 +59,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'vidhaan.urls'
@@ -154,6 +153,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'staticfiles'),
 # ]
