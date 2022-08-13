fae54461a7a91b494881b06329d79485fa5b576b
rahulkesharwani353 <rahulkesharwani353@gmail.com>
2022-08-13 21:00:02 +0530

    installed graph ql

--- vidhaan/settings.py
+++ vidhaan/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'petitionFiling',
     'django.contrib.staticfiles',  # required for serving swagger ui's css/js files
     'drf_yasg',
+    'graphene_django',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
