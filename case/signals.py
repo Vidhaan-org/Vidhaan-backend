d90eac9fa556e3eda0c0e638fdadb2ae830e0736
Sonal Jain <sonaljain067@gmail.com>
2022-08-16 16:59:30 +0530

    removing error temporarily

--- case/signals.py
+++ case/signals.py
@@ -19,7 +19,7 @@ account_sid="ACe3d5aa8ec8a8386da8303aa756091dcc"
 client = Client(account_sid, auth_token)
 
 
-    
+    # case.Case.None
 @receiver(post_save, sender=Notification)
 def notify_case_updates(sender, instance, created,**kwargs):
     if instance.notify_type=='New Case Update':
@@ -32,29 +32,29 @@ def notify_case_updates(sender, instance, created,**kwargs):
         ) 
 
         # sending sms
-        message = client.messages \
-        .create(
-            body=f"There is new update on the {instance.case}. Visit the website to get more info about it!'",
-            from_='+19786629400',
-            to='+918962245000'
-            )
+        # message = client.messages \
+        # .create(
+        #     body=f"There is new update on the {instance.case}. Visit the website to get more info about it!'",
+        #     from_='+19786629400',
+        #     to='+918962245000'
+        #     )
 
     if instance.notify_type=='Doc Deadline': 
         # sending mail
         send_mail(
             'Case Update',
-            f'Last date to submit your documents is {instance.case.case_document.recieving_date}',
+            f'It\'s Last date to submit your documents of case {instance.case}',
             'vidhaan.inbox@gmail.com',
             ['suryansh.1191@gmail.com', 'rahulkesharwani353@gmail.com', 'sonaljain067@gmail.com', 'dewansh.dt@gmail.com', 'emailanubhavagrawal@gmail.com']
         ) 
 
         # sending sms
-        message = client.messages \
-        .create(
-            body=f'Last date to submit your documents is {instance.case.case_document.recieving_date}',
-            from_='+19786629400',
-            to='+919179322789'
-            ) 
+        # message = client.messages \
+        # .create(
+            # body=f'It\'s Last date to submit your documents for your case {instance.case}',
+            # from_='+19786629400',
+            # to='+919179322789'
+            # ) 
         print('Doc Deadline')
 
     if instance.notify_type=='Hearing Update': 
@@ -67,49 +67,50 @@ def notify_case_updates(sender, instance, created,**kwargs):
         ) 
 
         # sending sms
-        message = client.messages \
-        .create(
-            body=f'There is new hearing update on the {instance.case}. Visit the website to get more info about it!',
-            from_='+19786629400',
-            to='+919179322789'
-            )
+        # message = client.messages \
+        # .create(
+        #     body=f'There is new hearing update on the {instance.case}. Visit the website to get more info about it!',
+        #     from_='+19786629400',
+        #     to='+919179322789'
+        #     )
         print('Hearing Update')
+
     if instance.notify_type=='Next Hearing': 
         # sending mail
         send_mail(
             'Case Update',
-            f'The next hearing date is on {instance.case.history.hearing_date}. Visit the website to get more info about it!',
+            f'The next hearing date for case {instance.case} is updated. Visit the website to get more info about it!',
             'vidhaan.inbox@gmail.com',
             ['suryansh.1191@gmail.com', 'rahulkesharwani353@gmail.com', 'sonaljain067@gmail.com', 'dewansh.dt@gmail.com', 'emailanubhavagrawal@gmail.com']
         ) 
 
         # sending sms
-        message = client.messages \
-        .create(
-            body=f'The next hearing date is on {instance.case.history.hearing_date}. Visit the website to get more info about it!',
-            from_='+19786629400',
-            to='+919179322789'
-            )
+        # message = client.messages \
+        # .create(
+        #     body=f'The next hearing date for case {instance.case} is updated. Visit the website to get more info about it!',
+        #     from_='+19786629400',
+        #     to='+919179322789'
+        #     )
 
         print('Next Hearing')
         
     if instance.notify_type=='Hearing in 2 days': 
-            # sending mail
-            send_mail(
-                'Case Update',
-                f'The next hearing date is on 2 days i.e {instance.case.history.hearing_date} of case {instance.case}. Visit the website to get more info about it!',
-                'vidhaan.inbox@gmail.com',
-                ['suryansh.1191@gmail.com', 'rahulkesharwani353@gmail.com', 'sonaljain067@gmail.com', 'dewansh.dt@gmail.com', 'emailanubhavagrawal@gmail.com']
-            ) 
+        # sending mail
+        send_mail(
+            'Case Update',
+            f'The next hearing date is on 2 days for the case {instance.case}. Visit the website to get more info about it!',
+            'vidhaan.inbox@gmail.com',
+            ['suryansh.1191@gmail.com', 'rahulkesharwani353@gmail.com', 'sonaljain067@gmail.com', 'dewansh.dt@gmail.com', 'emailanubhavagrawal@gmail.com']
+        ) 
 
-            # sending sms
-            message = client.messages \
-            .create(
-                body=f'The next hearing date is on 2 days i.e {instance.case.history.hearing_date} of case {instance.case}. Visit the website to get more info about it!',
-                from_='+19786629400',
-                to='+919179322789'
-                )
-            print('Hearing in 2 days')
+        # sending sms
+        # message = client.messages \
+        # .create(
+        #     body=f'The next hearing date is on 2 days for the case {instance.case}. Visit the website to get more info about it!',
+        #     from_='+19786629400',
+        #     to='+919179322789'
+        #     )
+        print('Hearing in 2 days')
 
 
 @receiver(post_save, sender=Petition)
