1cba40a8ea98dae6c8231c6ca6c335f97eff6fd5
Rahul Kesharwani <65972077+RahulKesharwani353@users.noreply.github.com>
2023-03-07 12:45:48 +0530

    Update settings.py

--- vidhaan/settings.py
+++ vidhaan/settings.py
@@ -32,13 +32,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://example.com",
-#     "https://sub.example.com",
-#     "http://localhost:8080",
-#     "http://127.0.0.1:9000",
-#     '*'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "https://vidhaan-backend-production.up.railway.app",
+    '*'
+]
 CORS_ORIGIN_WHITELIST = [
     'https://vidhaan-backend-production.up.railway.app'
 ]
